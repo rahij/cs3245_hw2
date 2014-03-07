4c5b62ea2bb0217da2bf2833607a3c71d3d57ae2
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2014-03-07 21:43:56 +0800

    fixed shit

--- search.py
+++ search.py
@@ -1,4 +1,3 @@
-#to parse postings list: http://stackoverflow.com/a/1926757/1805980
 #!/usr/bin/python
 import sys
 import getopt
@@ -62,9 +61,7 @@ def get_doc_ids_from_postings_file_at_pointer(file_pointer):
     doc_id = doc_ids[i]
     if does_doc_id_contain_skip_pointer(doc_id):
       doc_ids[i] = get_doc_id_from_doc_id_and_skip_pointer(doc_id)
-    doc_ids[i] = int(doc_ids[i])
   postings_file_reader.close()
-  # print doc_ids
   return doc_ids
 
 def write_to_output_file(line):
@@ -88,16 +85,15 @@ def execute_and_operation(operands):
   list_results = perform_query(operands[0].strip())
   for i in range(1, len(operands)):
     list_results = list(set(list_results) & set(perform_query(operands[i].strip())))
-  list_results.sort()
-  # print list_results
+  list_results.sort(key=int)
   return list_results
 
 def execute_or_operation(operands):
   list_results = []
   for operand in operands:
     list_results = list_results + perform_query(operand.strip())
-  list_results = sorted(list(set(list_results)))
-  # print list_results
+  list_results = list(set(list_results))
+  list_results.sort(key=int)
   return list_results
 
 def execute_not_operation(operand):
@@ -107,8 +103,8 @@ def execute_not_operation(operand):
   for doc_id in list_doc_ids_all:
     if doc_id not in list_operand_query_results:
       list_results.append(doc_id)
-  list_results = sorted(list(set(list_results)))
-  # print list_results
+  list_results = list(set(list_results))
+  list_results.sort(key=int)
   return list_results
 
 def are_there_brackets_in_expression(expr):
@@ -131,7 +127,7 @@ def get_expression_in_front_of_NOT(query):
   return matches[0]
 
 def perform_query(query):
-  # print query
+  global list_query_parantheses_results
   if are_there_brackets_in_expression(query):
     list_of_expressions_in_bracket = get_substrings_enclosed_in_brackets(query)
     for i in range(0, len(list_of_expressions_in_bracket)):
@@ -151,31 +147,21 @@ def perform_query(query):
   elif PREFIX_PARANTHESIS in query:
     index_query_in_bracket_list = get_index_of_bracketed_query(query)
     return list_query_parantheses_results[index_query_in_bracket_list]
-  # else:
-  #   if query in dictionary:
-  #     postings_file_pointer_for_query_term = int(dictionary[query])
-  #     return get_doc_ids_from_postings_file_at_pointer(postings_file_pointer_for_query_term)
-  #   elif string.lower(query) in dictionary:
-  #     postings_file_pointer_for_query_term = int(dictionary[string.lower(query)])
-  #     return get_doc_ids_from_postings_file_at_pointer(postings_file_pointer_for_query_term)
-  #   elif stemmer.stem(string.lower(query)) in dictionary:
-  #     postings_file_pointer_for_query_term = int(dictionary[stemmer.stem(string.lower(query))])
-  #     return get_doc_ids_from_postings_file_at_pointer(postings_file_pointer_for_query_term)
-  #   else:
-  #     raise Exception('Query ' + query + ' is not present in dictionary')
-  else:
+  elif stemmer.stem(string.lower(query)) in dictionary:
     postings_file_pointer_for_query_term = int(dictionary[stemmer.stem(string.lower(query))])
     return get_doc_ids_from_postings_file_at_pointer(postings_file_pointer_for_query_term)
-
-  # get_binary_operation_queue(query)
+  else:
+    return []
 
 def perform_queries():
   query_file_reader = open(query_file, 'r')
   postings_file_reader = open(postings_file, 'r')
   for query in query_file_reader.readlines():
+    global list_query_parantheses_results
     list_query_parantheses_results = []
     query = query.strip()
-    print perform_query(query)
+    res = perform_query(query)
+    write_to_output_file(" ".join(res))
 
 dict_file = postings_file = query_file = output_file = None
 try:
@@ -199,6 +185,7 @@ if query_file == None or dict_file == None or postings_file == None or output_fi
   sys.exit(2)
 
 dictionary = {}
+list_query_parantheses_results = []
 dictionary = store_dictionary_in_memory_and_return_it(dict_file)
 stemmer = nltk.stem.porter.PorterStemmer()
 perform_queries()
